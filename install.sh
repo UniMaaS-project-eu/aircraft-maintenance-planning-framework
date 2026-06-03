3b1fb35805f7af1c0dd0ca80f98b54116228ecc8
SeekerRook <xrist.vardakis@gmail.com>
2026-06-03 17:41:30 +0300

    minor fixes + install script

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
+python -m venv .venv
+source .venv/bin/activate
 git clone https://github.com/UniMaaS-project-eu/aircraft-level-planning-tcpn.git TCPN;
 git clone https://github.com/UniMaaS-project-eu/WP5-QUB-Aegean TACPN/TACPN_generator;
-#git clone [DATASETS] DATASETS
-#git clone [FLP_ALGO] FLP_ALGO
-#git clone [GROUPING] GROUPING
+pip install -r requirements.txt
+cd TCPN
+git submodule update --init --recursive
+cd cpn-py
+pip install -e .
