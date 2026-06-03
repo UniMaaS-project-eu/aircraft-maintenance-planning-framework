ba01db969761fd681bbd77d5fc433426fcb7844e
SeekerRook <xrist.vardakis@gmail.com>
2026-06-03 17:13:36 +0300

    added implementation of scheduling algorithms (grouping and flp)

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
-git clone git@github.com:UniMaaS-project-eu/aircraft-level-planning-tcpn.git TCPN;
+git clone https://github.com/UniMaaS-project-eu/aircraft-level-planning-tcpn.git TCPN;
 git clone https://github.com/UniMaaS-project-eu/WP5-QUB-Aegean TACPN/TACPN_generator;
-
+#git clone [DATASETS] DATASETS
+#git clone [FLP_ALGO] FLP_ALGO
+#git clone [GROUPING] GROUPING
