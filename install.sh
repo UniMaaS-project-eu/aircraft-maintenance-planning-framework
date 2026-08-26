56b3d2474bac14167442dd5717e69574f97ca867
SeekerRook <xrist.vardakis@gmail.com>
2026-08-26 15:35:01 +0300

    updated install.sh (exit on error)

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-python -m venv .venv
-source .venv/bin/activate
-git clone https://github.com/UniMaaS-project-eu/aircraft-level-planning-tcpn.git TCPN;
-git clone https://github.com/UniMaaS-project-eu/WP5-QUB-Aegean TACPN/TACPN_generator;
-pip install -r requirements.txt
-cd TCPN
-git submodule update --init --recursive
-cd cpn-py
+python3 -m venv .venv &&
+source .venv/bin/activate &&
+git clone https://github.com/UniMaaS-project-eu/aircraft-level-planning-tcpn.git TCPN &&
+git clone https://github.com/UniMaaS-project-eu/WP5-QUB-Aegean TACPN/TACPN_generator &&
+pip install -r requirements.txt &&
+cd TCPN &&
+git submodule update --init --recursive &&
+cd cpn-py &&
 pip install -e .
